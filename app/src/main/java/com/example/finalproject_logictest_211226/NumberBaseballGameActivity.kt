324682574a78cbcb63ab76fc13bab44bdfd64de1
이진호 <spris1513@gmail.com>
2021-12-26 14:14:09 +0900

    3개의 랜덤 숫자 출제 함수 추가

--- app/src/main/java/com/example/finalproject_logictest_211226/NumberBaseballGameActivity.kt
+++ app/src/main/java/com/example/finalproject_logictest_211226/NumberBaseballGameActivity.kt
@@ -16,6 +16,9 @@ class NumberBaseballGameActivity : BaseActivity() {
 
     lateinit var mAdapter : ChatAdapter
 
+//    컴퓨터가 낸 3자리 문제를 담아둘 ArrayList
+    val cpuNumbers = ArrayList<Int>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this,R.layout.activity_number_baseball_game)
@@ -47,6 +50,8 @@ class NumberBaseballGameActivity : BaseActivity() {
 
     override fun setValues() {
 
+        makeCpuNumbers()
+
         mChatList.add(ChatData("CPU","숫자 야구게임에 오신것을 환영합니다."))
         mChatList.add(ChatData("CPU","3자리 숫자로 문제가 생성되었습니다."))
         mChatList.add(ChatData("CPU","밑의 입력칸을 이용해 3자리 숫자를 맞춰주세요."))
@@ -56,4 +61,33 @@ class NumberBaseballGameActivity : BaseActivity() {
         binding.chattingRecyclerView.layoutManager = LinearLayoutManager(mContext)
 
     }
+
+//    컴퓨터가 3자리 문제 출제 함수
+
+    fun makeCpuNumbers(){
+
+//        3개의 숫자를 문제로 제출
+        for ( i in 0 until 3 ) {
+
+//            써도 되는 숫자가 나올때까지 무한반복 > 랜덤숫자 추출
+            while (true){
+
+//                1~9의 랜덤값 추출
+                val randomNum = (Math.random()*9+1 ).toInt()  // 1~9 사이의 랜덤값.
+
+//                cpuNumber에 이미 나와있냐?
+                val isDuplOk = !cpuNumbers.contains(randomNum)
+
+                if(isDuplOk){
+                    cpuNumbers.add(randomNum)
+//                    무한반복 종료 > 다음숫자 뽑으러.
+                    break
+                }
+
+            }
+
+        }
+
+    }
+
 }
