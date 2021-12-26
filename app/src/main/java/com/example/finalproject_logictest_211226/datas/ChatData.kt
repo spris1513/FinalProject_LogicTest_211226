ad4cabd43d7589e57cccd3c0b147f6750f343c71
이진호 <spris1513@gmail.com>
2021-12-26 13:46:35 +0900

    채팅 데이터 표현용 클래스

---
+++ app/src/main/java/com/example/finalproject_logictest_211226/datas/ChatData.kt
@@ -0,0 +1,7 @@
+package com.example.finalproject_logictest_211226.datas
+
+class ChatData(
+    var owner : String, // 'CPU' or 'USER' 둘 중 하나로 담아서 누가 말한건지 기록.
+    var message : String, // 실제 보낸 내용
+) {
+}
