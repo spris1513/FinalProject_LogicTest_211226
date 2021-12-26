96cc2221a8bcb46cdc760ae59740ebc563276616
이진호 <spris1513@gmail.com>
2021-12-26 12:40:18 +0900

    자동구매버튼 클릭시 > 반복 구매 프로세스 시작

--- app/src/main/java/com/example/finalproject_logictest_211226/LottoSimulatorActivity.kt
+++ app/src/main/java/com/example/finalproject_logictest_211226/LottoSimulatorActivity.kt
@@ -82,17 +82,10 @@ class LottoSimulatorActivity : BaseActivity() {
         
         binding.btnAutoMode.setOnClickListener { 
             
-//            임시코드. 소지 금액이 0이 될 때까지 반복.
-            while (true){
-                makeWinNumbers()
-                checkLottoRank()
-                
-                if (myMoney <= 0){
-//                    돈을 다 쓴 상황
-                    Toast.makeText(mContext, "소지 금액을 모두 사용했습니다.", Toast.LENGTH_SHORT).show()
-                    break
-                }
-            }
+//              반복구매 프로세스를 변수에 저장해둠(buyLottoRunnable)
+//            myHandler가 해당 프로세스를 시작하도록
+
+            myHandler.post(buyLottoRunnable)
             
         }
 
