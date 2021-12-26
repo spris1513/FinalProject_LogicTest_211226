84e37329f7defaa98ae0103b79a16e304b0e04ab
이진호 <spris1513@gmail.com>
2021-12-26 10:36:51 +0900

    내 번호 / 당첨번호 저장 공간 세팅

--- app/src/main/java/com/example/finalproject_logictest_211226/LottoSimulatorActivity.kt
+++ app/src/main/java/com/example/finalproject_logictest_211226/LottoSimulatorActivity.kt
@@ -9,6 +9,12 @@ class LottoSimulatorActivity : BaseActivity() {
 
     lateinit var binding : ActivityLottoSimulatorBinding
 
+//    내 입력 번호 목록
+    val myNumberList = arrayListOf(5,10,12,15,16,24)
+
+//    랜덤 당첨번호 목록 > 나중에 6개를 채워야 함.
+    val winNumBerList = ArrayList<Int>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this,R.layout.activity_lotto_simulator)
